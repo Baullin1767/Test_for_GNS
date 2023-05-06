3da6a3cc532887a5c7277888b2267c5ce83703bf
Baullin1767 <86959616+Baullin1767@users.noreply.github.com>
2023-05-06 17:19:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Form
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Form, status
+from fastapi.responses import FileResponse, JSONResponse
 import requests
  
 app = FastAPI()
@@ -7,22 +7,41 @@ app = FastAPI()
 @app.get("/")
 def root():
     return FileResponse("public/index.html")
+
+users={}
  
-# метод post для клтиента
+# метод post для клиента
 @app.post("/postdata")
 def postdata(username = Form(), userphone=Form()):
-    # Отправляем post запрос на сервис
-    requests.post('https://order.drcash.sh/v1/order', 
-                  data={'title':{
-                                'Content-Type': 'application/json',
-                                'Authorization': 'Bearer RLPUUOQAMIKSAB2PSGUECA'
-                                },
-                        'body':{
-                                'stream_code': 'vv4uf',
-                                'client': {
-                                            'phone': userphone,
-                                            'name': username,
-                                        }
-                            }
-                    }
-                )
+    # Проверяем, есть ли пользователь в списке
+    if not user_exist(username):
+        users.update({username: userphone})  # Если нет добавляем пользователя в список
+        # И отправляем post запрос на сервис
+        requests.post('https://order.drcash.sh/v1/order', 
+                      data={'title':{
+                                    'Content-Type': 'application/json',
+                                    'Authorization': 'Bearer RLPUUOQAMIKSAB2PSGUECA'
+                                    },
+                            'body':{
+                                    'stream_code': 'vv4uf',
+                                    'client': {
+                                                'phone': userphone,
+                                                'name': username,
+                                            }
+                                }
+                        }
+                    )
+        return {username: userphone}
+    else:
+        # Если пользователь найден отправляем статус код и сообщение
+        return JSONResponse(
+                status_code=status.HTTP_208_ALREADY_REPORTED, 
+                content="Пользователь уже существует"
+        )
+
+# Проверяет существует ли пользователь
+def user_exist(username):
+    if username in users:
+        return True
+    else:
+        return False
